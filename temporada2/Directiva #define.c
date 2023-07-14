e96575dc0b4dbfa91af3c11c50059269e9c5ce9a
Creator125 <85368990+Creator125@users.noreply.github.com>
2023-07-14 14:50:16 -0500

    Temporada 2/: Areglando 'directiva #define.c'

--- temporada2/Directiva #define.c
+++ temporada2/Directiva #define.c
@@ -7,7 +7,7 @@ a) Realizar la carga de la información mencionada.
 b) Generar un vector que contenga el ingreso acumulado en sueldos en los últimos 3 meses para cada empleado.
 c) Mostrar por pantalla el total pagado en sueldos a todos los empleados en los últimos 3 meses
 d) Obtener el nombre del empleado que tuvo el mayor ingreso acumulado
-Utilizar macros para definir la cantidad de filas y columnas de las estructuras de datos.
+Utilizar macros para definir la cantidad de CANTEMPLEADOS y MESES de las estructuras de datos.
 
 Imangen de referencia: https://www.tutorialesprogramacionya.com/cya/imagentema/foto056.jpg
 
@@ -17,16 +17,16 @@ https://www.youtube.com/watch?v=NvSJNXXJgdE&list=PLIygiKpYTC_6zHLTjI6cFIRZm1BCT3
 #include <stdio.h>
 #include <string.h>
 
-#define FILAS 4
-#define COLUMNAS 3
+#define CANTEMPLEADOS 4 //Cantidad de empleados
+#define MESES 3 //Meses
 
-void cargarDatos(char empleados[FILAS][40], float sueldos[FILAS][COLUMNAS]){
+void cargarDatos(char empleados[CANTEMPLEADOS][40], float sueldos[CANTEMPLEADOS][MESES]){
 
-  for (int f = 0; f < FILAS; f++){ //Ingresando los datos
+  for (int f = 0; f < CANTEMPLEADOS; f++){ //Ingresando los datos
     printf("Ingrese el nombre del empleado: ");
     gets(empleados[f]);
 
-    for (int c = 0; c < COLUMNAS; c++){ //Ingresando los sueldos del empleado
+    for (int c = 0; c < MESES; c++){ //Ingresando los sueldos del empleado
         printf("Ingrese el sueldo del empleado: $");
         scanf("%f",&sueldos[f][c]);
     }
@@ -37,11 +37,11 @@ void cargarDatos(char empleados[FILAS][40], float sueldos[FILAS][COLUMNAS]){
 }
 
 
-void calcularSueldoTotal(float sueldos[FILAS][COLUMNAS], float sueldototal[FILAS]){ //Funcion para generar la matriz que obtendrá el sueldo total de cada empleado
-    for (int f = 0; f < FILAS; f++){
+void calcularSueldoTotal(float sueldos[CANTEMPLEADOS][MESES], float sueldototal[CANTEMPLEADOS]){ //Funcion para generar la matriz que obtendrá el sueldo total de cada empleado
+    for (int f = 0; f < CANTEMPLEADOS; f++){
         float suma = 0;
 
-        for (int c = 0; c < COLUMNAS; c++){
+        for (int c = 0; c < MESES; c++){
             suma += sueldos[f][c]; //Sumando todos los sueldos totales de cada empleados
         }
         sueldototal[f] = suma; //Asignando el valor sumado al sueldo total
@@ -49,32 +49,34 @@ void calcularSueldoTotal(float sueldos[FILAS][COLUMNAS], float sueldototal[FILAS
 }
 
 
-void imprimirPagos(char empleados[FILAS][40], float sueldototal[FILAS]){
-    for (int f = 0; f < FILAS; f++){
+void imprimirPagos(char empleados[CANTEMPLEADOS][40], float sueldototal[CANTEMPLEADOS]){
+    printf("El total pagado en sueldos en los ultimos 3 meses.\n");
+
+    for (int f = 0; f < CANTEMPLEADOS; f++){
         printf("%s: $%.2f\n",empleados[f],sueldototal[f]);
     }
 }
 
 
-void empleadoConMayorIngreso(char empleados[FILAS][40], float sueldototal[FILAS]){
+void empleadoConMayorIngreso(char empleados[CANTEMPLEADOS][40], float sueldototal[CANTEMPLEADOS]){
     char empleadomayor[40];
     float mayor = sueldototal[0];
     strcpy(empleadomayor,empleados[0]);
 
-    for (int f = 1; f < FILAS; f++){
+    for (int f = 1; f < CANTEMPLEADOS; f++){
         if (sueldototal[f] > mayor){
             mayor = sueldototal[f];
             strcpy(empleadomayor,empleados[f]);
         }
     }
 
-    printf("\nEl empleado con mayor ingreso es %s\n",empleadomayor);
+    printf("\nEl empleado con mayor ingreso es %s con %.2f\n",empleadomayor, mayor);
 }
 
 
 int main(){
-    char empleados[FILAS][40];
-    float sueldos[FILAS][COLUMNAS], sueldototal[FILAS];
+    char empleados[CANTEMPLEADOS][40];
+    float sueldos[CANTEMPLEADOS][MESES], sueldototal[CANTEMPLEADOS];
 
     cargarDatos(empleados,sueldos);
     calcularSueldoTotal(sueldos,sueldototal);
